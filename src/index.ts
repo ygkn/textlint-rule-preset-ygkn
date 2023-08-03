31df4d72c159e56f9dcfab8345af4bda35f5b201
ygkn <ygkn@users.noreply.github.com>
2023-08-04 05:40:14 +0900

    make rules preset

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,8 @@
-import type { TextlintRuleModule } from "@textlint/types";
+import type { TextlintConfigRulePreset } from "@textlint/config-loader";
 
-export interface Options {
-  // If node's text includes allowed text, does not report.
-  allows?: string[];
-}
+const config = {
+  rules: {},
+  rulesConfig: {},
+} satisfies TextlintConfigRulePreset["preset"];
 
-const report: TextlintRuleModule<Options> = (context, options = {}) => {
-  const { Syntax, RuleError, report, getSource, locator } = context;
-  const allows = options.allows ?? [];
-  return {
-    [Syntax.Str](node) {
-      // "Str" node
-      const text = getSource(node); // Get text
-      if (allows.some((allow) => text.includes(allow))) {
-        return;
-      }
-      const matches = text.matchAll(/bugs/g);
-      for (const match of matches) {
-        const index = match.index ?? 0;
-        const matchRange = [index, index + match[0].length] as const;
-        const ruleError = new RuleError("Found bugs.", {
-          padding: locator.range(matchRange),
-        });
-        report(node, ruleError);
-      }
-    },
-  };
-};
-
-export default report;
+export default config;
